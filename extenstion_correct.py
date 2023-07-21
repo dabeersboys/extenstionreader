ec3ec9ab0ffb86efa9aff05eda854d423e2de031
dabeersboys <68974202+dabeersboys@users.noreply.github.com>
2023-07-21 09:12:02 -0600

    Update extenstion_correct.py
    fixed with open(filename) to allow the next process to kick off. Thanks Charlie!

--- extenstion_correct.py
+++ extenstion_correct.py
@@ -4,33 +4,34 @@ def add_file_extension(filename):
     jpg_header = b'\xFF\xD8\xFF\xE0'
     png_header = b'\x89\x50\x4E\x47'
 
-    with open(filename, 'rb') as file:
-        header = file.read(4)
+    with open(filename, 'rb') as file:                                  
+        header = file.read(4)                                          
+        if header.startswith(jpg_header):                               
+            new_extension = '.jpg'                                      
+        elif header.startswith(png_header):                                
+            new_extension = '.png'                                      
+        else:                                                           
+            print(f"Unrecognized file format. Skipping {filename}")     
+            return                                                      
 
-        if header.startswith(jpg_header):
-            new_extension = '.jpg'
-        elif header.startswith(png_header):
-            new_extension = '.png'
-        else:
-            print(f"Unrecognized file format. Skipping {filename}")
-            return
+    directory_path, old_basename = os.path.split(filename)
+    new_basename = os.path.splitext(old_basename)[0] + new_extension
+    new_filename = os.path.join(directory_path, new_basename)
 
-        directory_path, old_basename = os.path.split(filename)
-        new_basename = os.path.splitext(old_basename)[0] + new_extension
-        new_filename = os.path.join(directory_path, new_basename)
-
-        print(f"Detected {new_extension[1:].upper()} file. Renaming to: {new_basename}")
-        if not os.path.exists(new_filename):
-            try:
-                os.rename(filename, new_filename)
-            except OSError as e:
-                print(f"Error renaming {filename}: {e}")
-        else:
-            print(f"File {new_basename} already exists. Skipping renaming.")
+    print(f"Detected {new_extension[1:].upper()} file. Renaming to: {new_basename}")
+    if not os.path.exists(new_filename):
+        # try:
+            os.rename(filename, new_filename)                           
+        # except OSError as e:                                          
+        #     print(f"Error renaming {filename}: {e}")
+    else:
+        print(f"File {new_basename} already exists. Skipping renaming.")
 
 
 def process_directory(directory_path):
+    print(os.listdir(directory_path))
     for filename in os.listdir(directory_path):
+        print(filename)
         full_path = os.path.join(directory_path, filename)
         if os.path.isfile(full_path):
             add_file_extension(full_path)
@@ -39,5 +40,6 @@ def process_directory(directory_path):
 
 
 if __name__ == "__main__":
-    directory_path = input("What is the path to the directory to search? ")
-    process_directory(directory_path)
+    direct_path = input("What is the path to the directory to search? ")
+    # print(directory_path)
+    process_directory(direct_path)
